12d982b5d4e07aaf1ba7c0d2a41227ebe1b82018
Alex Besleaga <sanyadevel@gmail.com>
2023-05-20 23:14:57 +0300

    refactoring

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,18 +1,18 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
+import userProfileInfo from '../slices/userProfileInfo';
 import { articlesApi } from '../slices/getArticlesFromApi';
 import articleSlice from '../slices/articleSlice';
 import registerUser from '../slices/userRegistration';
 import userLogin from '../slices/userLogin';
 import getFullArticle from '../slices/fullArticlePage';
-import postArticle from '../slices/postAnArticle';
-import deleteArticle from '../slices/deleteArticle';
-import getCurrentUser from '../slices/getUser';
-import userProfileInfo from '../slices/userProfileInfo';
-import likeArticle from '../slices/likeAnArticle';
-import dislikeArticle from '../slices/dislikeAnArticle';
-import editUserInfo from '../slices/editUserInfo';
 import editArticle from '../slices/editAnArticle';
+import editUserInfo from '../slices/editUserInfo';
+import dislikeArticle from '../slices/dislikeAnArticle';
+import getCurrentUser from '../slices/getUser';
+import postArticle from '../slices/postAnArticle';
+import likeArticle from '../slices/likeAnArticle';
+import deleteArticle from '../slices/deleteArticle';
 
 export const rootReducer = combineReducers({
   articlesApi: articlesApi.reducer,
